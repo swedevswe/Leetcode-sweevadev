9a78b261848c9a190b778d39743fa5634df37003
Swetha Sebastian <90139229+SWEdatCode@users.noreply.github.com>
2024-02-21 17:51:09 +0000

    Time: 1 ms (46.99%), Space: 40.9 MB (41.08%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        
-      if(n==1){
-          return true;
-      }else if(n<=0 || n % 2 != 0){
-          return false;
-      }
-        return isPowerOfTwo(n/2);
+      return n > 0 && (n & (n-1)) == 0;
     }
 }
