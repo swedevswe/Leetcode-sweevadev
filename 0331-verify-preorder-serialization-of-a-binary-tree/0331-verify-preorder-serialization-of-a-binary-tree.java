ad1f8065eb42ed4a187f7cc3b4392fdbeda20040
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-02-23 21:38:27 +0000

    Time: 4 ms (91.37%), Space: 43.3 MB (82.10%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int slots = 1;
+        
+        for(String node : nodes){
+            slots--;
+            
+            if(slots<0) return false;
+            if(!node.equals("#")){
+                slots += 2;
+            }
+        }
+        
+        return slots == 0;
+        
+    }
+}
