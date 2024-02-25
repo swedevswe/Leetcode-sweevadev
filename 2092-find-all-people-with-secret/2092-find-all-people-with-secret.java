b127be518c73cea5c0172b5056db9444c552ac49
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-02-25 00:40:05 +0000

    Time: 96 ms (45.00%), Space: 100 MB (69.00%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,78 +1,88 @@
 class Solution {
-   public static List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-
-        int maxMeetTime = 0;
-
-        for(int i = 0; i < meetings.length; i++) maxMeetTime = Math.max(maxMeetTime, meetings[i][2]);
-
-        List<List<int[]>> list = new ArrayList<List<int[]>>();
-        for(int i = 0; i <= maxMeetTime; i++ ) {
-            list.add(new ArrayList<int[]>());
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        // Sort meetings by time to ensure chronological processing.
+        Map<Integer, List<Integer>> meetingsByTime = new TreeMap<>();
+        for (int i = 0; i < meetings.length; i++) {
+            meetingsByTime.computeIfAbsent(meetings[i][2], k -> new ArrayList<>()).add(i);
         }
-
-        for(int i = 0; i < meetings.length; i++) {
-            int time = meetings[i][2];
-            int a = meetings[i][0];
-            int b = meetings[i][1];
-
-            list.get(time).add(new int[]{a,b});
-        }
-
         
-
-        int parent[] = new int[n];
-        for(int i = 0; i < parent.length; i++) parent[i] = i;
-        parent[firstPerson] = 0;
+        // Initialize Union-Find structure for tracking connections.
+        UnionFind uf = new UnionFind(n);
+        // Initially, person 0 and firstPerson are connected, meaning they know the secret.
+        uf.union(0, firstPerson);
         
-        for(int i = 0; i < list.size(); i++) {
-            if ( list.get(i).size() == 0) continue;
+        // Process each set of meetings occurring at the same time.
+        for (int time : meetingsByTime.keySet()) {
+            Set<Integer> currentPool = new HashSet<>();
             
-            for(int j = 0; j < list.get(i).size(); j++) {
-                int a = list.get(i).get(j)[0];
-                int b = list.get(i).get(j)[1];
-
-                union(a, b, parent);
+            // Union everyone involved in the current time slot's meetings.
+            for (int index : meetingsByTime.get(time)) {
+                int[] meeting = meetings[index];
+                uf.union(meeting[0], meeting[1]);
+                currentPool.add(meeting[0]);
+                currentPool.add(meeting[1]);
             }
-
-            for(int j = 0; j < list.get(i).size(); j++) {
-                int a = list.get(i).get(j)[0];
-                int b = list.get(i).get(j)[1];
-                find(a, parent);
-                find(b, parent);
-
-                if ( parent[a] != 0 ) parent[a] = a;
-                if ( parent[b] != 0 ) parent[b] = b;
-            }
-
             
-        }
-
-        List<Integer> result = new ArrayList<Integer>();
-        for(int i = 0; i < parent.length; i++) {
-            find(i, parent);
-
-            if (parent[i] == 0 ) result.add(i);
-        }
-
-        return result;
-    }
-
-
-    private static int find(int a, int[] parent) {
-        if ( a == parent[a] ) return a;
-        return parent[a] = find(parent[a], parent);
-    }
-
-    private static void union(int a, int b, int[] parent) {
-        a = find(a, parent);
-        b = find(b, parent);
-        
-        if ( a != b) {
-            if ( a <= b) {
-                parent[b] = a;
-            } else{
-                parent[a] = b;
+            // If a person in the current pool isn't connected to 0, reset their connection.
+            // This implies they don't know the secret after this round of meetings.
+            for (int person : currentPool) {
+                if (!uf.isConnected(0, person)) {
+                    uf.reset(person);
+                }
             }
+        }
+        
+        // Collect and return all people connected to 0, as they know the secret.
+        List<Integer> peopleWhoKnowSecret = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (uf.isConnected(i, 0)) {
+                peopleWhoKnowSecret.add(i);
+            }
+        }
+        return peopleWhoKnowSecret;
+    }
+    
+    class UnionFind {
+        int[] parent, rank;
+
+        public UnionFind(int size) {
+            parent = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i; // Initially, everyone is their own parent.
+                rank[i] = 0; // Initial rank of trees is 0.
+            }
+        }
+        
+        public int find(int person) {
+            if (person != parent[person]) {
+                parent[person] = find(parent[person]); // Path compression.
+            }
+            return parent[person];
+        }
+        
+        public void union(int person1, int person2) {
+            int root1 = find(person1);
+            int root2 = find(person2);
+            if (root1 != root2) {
+                if (rank[root1] < rank[root2]) {
+                    parent[root1] = root2;
+                } else if (rank[root1] > rank[root2]) {
+                    parent[root2] = root1;
+                } else {
+                    parent[root2] = root1;
+                    rank[root1]++;
+                }
+            }
+        }
+        
+        public boolean isConnected(int person1, int person2) {
+            return find(person1) == find(person2);
+        }
+        
+        public void reset(int person) {
+            parent[person] = person;
+            rank[person] = 0;
         }
     }
 }
