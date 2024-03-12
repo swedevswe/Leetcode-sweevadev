de389013efea064a049036323c440167cec3ed8b
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-12 20:12:16 +0000

    Time: 0 ms (100.00%), Space: 41.4 MB (65.54%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,22 +1,26 @@
 class Solution {
     public String customSortString(String order, String s) {
+        
         int[] count = new int[26];
+        
         for(char c : s.toCharArray()){
-            count[c - 'a']++;
-        }
+            count[c-'a']++;
+            }
+            
         StringBuilder result = new StringBuilder();
         for(char c : order.toCharArray()){
-            while(count[c - 'a']>0){
-                result.append(c);
-                count[c - 'a']--;
-            }
-        }
-        for(char c = 'a'; c<= 'z'; c++){
             while(count[c-'a']>0){
                 result.append(c);
                 count[c-'a']--;
             }
         }
-        return result.toString();
+        for(char c = 'a'; c<='z'; c++){
+            while(count[c-'a']>0){
+                result.append(c);
+                count[c-'a']--;
+            
+        }
     }
+        return result.toString();
+}
 }
