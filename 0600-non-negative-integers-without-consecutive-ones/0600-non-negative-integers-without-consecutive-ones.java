ce0250ff76ea9eaf9572dab84ec33f16312110a4
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-14 19:28:35 +0000

    Time: 1 ms (91.77%), Space: 40.6 MB (75.32%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
@@ -1,29 +1,30 @@
 class Solution {
     public int findIntegers(int n) {
-        int fib[] = new int[32];
+        int[] fib = new int[32];
         fib[0] = 1;
         fib[1] = 2;
-        
         for(int i = 2; i<32; i++){
-            fib[i] = fib[i-1] + fib[i-2];   
+            fib[i] = fib[i-1] + fib[i-2];
         }
-        int result = 0;
         int k = 30;
-        
-        boolean prev_bit_one = false;
-        while(k>=0){
-            if((n&(1<<k))!=0){
+        int result = 0;
+                   
+     boolean prevBitOne = false;
+                       while(k>=0){
+                           if((n&(1<<k))!=0){
                 result += fib[k];
-                if(prev_bit_one){
+                if(prevBitOne){
                     result--;
                     break;
-                }prev_bit_one = true;
-            }else{
-                    prev_bit_one = false;
                 }
-                k--;
-            }
-            return result + 1;
+                prevBitOne = true;
+            }else{
+                               prevBitOne = false;
+                               
+                           }
+                              k--;
+            
         }
-        
+        return result + 1;
     }
+}
