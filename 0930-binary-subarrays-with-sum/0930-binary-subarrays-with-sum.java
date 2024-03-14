4fb0b6598a9e66cd3f4155f1105446c2d262bf80
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-14 20:00:24 +0000

    Time: 23 ms (32.64%), Space: 52.7 MB (7.16%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -5,16 +5,16 @@ class Solution {
         
         HashMap<Integer, Integer> prefixSum = new HashMap<>();
         prefixSum.put(0,1);
-        
         for(int num : nums){
             sum = sum + num;
             
+            
             if(prefixSum.containsKey(sum-goal)){
                 count = count + prefixSum.get(sum-goal);
+                
             }
             prefixSum.put(sum, prefixSum.getOrDefault(sum, 0) + 1);
         }
         return count;
-        
     }
 }
