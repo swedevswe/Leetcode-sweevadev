a2a5101c25ae3231dbcc3c6ebaca6b96c1c05263
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-16 14:01:26 +0000

    Time: 1 ms (86.61%), Space: 41.3 MB (53.15%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.java
+++ 2451-odd-string-difference/2451-odd-string-difference.java
@@ -1,19 +1,20 @@
 class Solution {
     public String oddString(String[] words) {
         int[][] matrix = new int[words.length][words[0].length()-1];
-        for(int i = 0; i<words.length; i++){
-            String word = words[i];
-            for(int j = 0; j<word.length()-1; j++){
-                matrix[i][j] = word.charAt(j+1) - word.charAt(j);
+            for(int i = 0; i<words.length; i++){
+                String word = words[i];
+                for(int j = 0; j<word.length()-1; j++){
+                    matrix[i][j] = word.charAt(j+1) - word.charAt(j);
+                }
             }
-        }
-        if(!Arrays.equals(matrix[0], matrix[1])){
-            if(Arrays.equals(matrix[1], matrix[2])){
-                return words[0];
-            }else{
-                return words[1];
+            
+            if(!Arrays.equals(matrix[0], matrix[1])){
+                if(Arrays.equals(matrix[1], matrix[2])){
+                    return words[0];
+                }else{
+                    return words[1];
+                }
             }
-        }
         for(int i = 1; i<matrix.length; i++){
             if(!Arrays.equals(matrix[i], matrix[i-1])){
                 return words[i];
