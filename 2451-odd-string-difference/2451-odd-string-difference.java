9d31ac7c06a2d703f5c4c15d2efc69020784d1e4
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-16 10:48:40 +0000

    Time: 1 ms (86.61%), Space: 41.2 MB (60.63%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.java
+++ 2451-odd-string-difference/2451-odd-string-difference.java
@@ -1,22 +1,21 @@
 class Solution {
     public String oddString(String[] words) {
-        int[][] matrix = new int[words.length][words[0].length()];
+        int[][] matrix = new int[words.length][words[0].length()-1];
         for(int i = 0; i<words.length; i++){
             String word = words[i];
             for(int j = 0; j<word.length()-1; j++){
                 matrix[i][j] = word.charAt(j+1) - word.charAt(j);
             }
         }
-        for(int i = 1; i<matrix.length-1; i++){
-            int[] prev = matrix[i-1], cur = matrix[i], next = matrix[i+1];
-            
-            if(Arrays.equals(prev, cur) && !Arrays.equals(cur, next)){
-                return words[i+1];
+        if(!Arrays.equals(matrix[0], matrix[1])){
+            if(Arrays.equals(matrix[1], matrix[2])){
+                return words[0];
+            }else{
+                return words[1];
             }
-             if(Arrays.equals(cur, next) && !Arrays.equals(cur, prev)){
-                return words[i-1];
-            }
-             if(Arrays.equals(prev, next) && !Arrays.equals(cur, prev)){
+        }
+        for(int i = 1; i<matrix.length; i++){
+            if(!Arrays.equals(matrix[i], matrix[i-1])){
                 return words[i];
             }
         }
