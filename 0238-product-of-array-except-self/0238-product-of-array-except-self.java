4435535e8d6a2c057e0569db63287cf9822902ab
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-17 09:12:38 +0000

    Time: 1 ms (100.00%), Space: 53.1 MB (68.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,17 +4,14 @@ class Solution {
         
         int left = 1;
         for(int i = 0; i<nums.length; i++){
-                output_arr[i] = left;
-                left = nums[i] * left;
-                
-            }
-        
+            output_arr[i] = left;
+            left = nums[i] * left;
+        }
         int right = 1;
         for(int i = nums.length-1; i>=0; i--){
-                output_arr[i] = output_arr[i] * right;
-                right = nums[i] * right;
-            }
-        
+            output_arr[i] = output_arr[i] * right;
+            right = nums[i] * right;
+        }
         return output_arr;
     }
 }
