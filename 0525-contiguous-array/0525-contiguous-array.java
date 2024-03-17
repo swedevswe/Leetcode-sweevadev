1ba9809174e1b95e10d71d39bae6e53f3dad31bb
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-17 08:32:18 +0000

    Time: 24 ms (57.01%), Space: 55.4 MB (20.82%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -6,17 +6,13 @@ class Solution {
         int count = 0;
         int maxLength = 0;
         for(int i = 0; i<nums.length; i++){
-            count = count + (nums[i]==0 ? -1 : 1);
+            count = count + (nums[i]==1 ? -1 : 1);
             if(map.containsKey(count)){
                 maxLength = Math.max(maxLength, i - map.get(count));
             }else{
                 map.put(count, i);
             }
-            
         }
-        return maxLength;
-               
-        
-        
+        return maxLength;  
     }
 }
