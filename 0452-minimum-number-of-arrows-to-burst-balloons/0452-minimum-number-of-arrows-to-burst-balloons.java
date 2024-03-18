4481cf8d2edd6d6b1d8628ef49d8ac135a61da89
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-18 20:05:48 +0000

    Time: 55 ms (53.64%), Space: 68.6 MB (77.08%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -4,15 +4,15 @@ class Solution {
             return 0;
         }
         Arrays.sort(points, (a,b) -> Integer.compare(a[1], b[1]));
-        int arrows = 1;
+        int arrowCount = 1;
         int arrowPos = points[0][1];
         
         for(int i = 1; i<points.length; i++){
             if(points[i][0] > arrowPos){
-                arrows++;
+                arrowCount++;
                 arrowPos = points[i][1];
             }
         }
-        return arrows;
+        return arrowCount;
     }
 }
