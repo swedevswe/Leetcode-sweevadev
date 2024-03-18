05d9db124d81a29d896db6a6a6203fb89a8ffa1e
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-18 19:45:46 +0000

    Time: 1 ms (98.60%), Space: 44.7 MB (60.27%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -6,7 +6,7 @@ class Solution {
             result.add(intervals[i]);
             i++;
         }
-        while(i<intervals.length && intervals[i][0]<=newInterval[1]){
+        while(i<intervals.length&& intervals[i][0]<=newInterval[1]){
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
@@ -16,8 +16,8 @@ class Solution {
         while(i<intervals.length){
             result.add(intervals[i]);
             i++;
-            
         }
         return result.toArray(new int[result.size()][]);
+        
     }
 }
