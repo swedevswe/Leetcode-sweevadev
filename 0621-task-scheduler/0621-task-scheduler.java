523a1c0bd1050417de720ab4022d74a0b142557b
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-19 20:14:49 +0000

    Time: 3 ms (83.64%), Space: 44.9 MB (96.31%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,29 +1,24 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for(char task : tasks){
-            freqMap.put(task, freqMap.getOrDefault(task,0)+1);
-        }
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        maxHeap.addAll(freqMap.values());
+        int[] counter = new int[26];
+        int maximum = 0;
+        int maxCount = 0;
         
-        int cycles = 0;
-        while(!maxHeap.isEmpty()){
-            List<Integer>temp = new ArrayList<>();
-            for(int i = 0; i<=n; i++){
-                if(!maxHeap.isEmpty()){
-                    temp.add(maxHeap.remove());
-                }
+        for(char task : tasks){
+            counter[task - 'A']++;
+            if(maximum == counter[task - 'A']){
+                maxCount++;
+            }else if(maximum < counter[task - 'A']){
+                maximum = counter[task - 'A'];
+                maxCount = 1;
+                
             }
-            for(int freq : temp){
-                if(--freq > 0){
-                    maxHeap.add(freq);
-                }
-            }
-            cycles += maxHeap.isEmpty() ? temp.size(): n+1;
-            
         }
-        return cycles;
-            
-        }
+        int partCount = maximum - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - maximum * maxCount;
+        int idles = Math.max(0, emptySlots - availableTasks);
+        return tasks.length + idles;
     }
+}
