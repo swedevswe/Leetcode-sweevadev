ff416bb4a5448cbc24eeba25fa36b72c599226e6
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-20 18:50:10 +0000

    Time: 3 ms (83.64%), Space: 45.4 MB (75.83%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,24 +1,27 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        
+        //counter, maximum, maxCount, idles, partlength, partCount, available tasks, empty slots
+        
         int[] counter = new int[26];
         int maximum = 0;
         int maxCount = 0;
         
         for(char task : tasks){
-            counter[task - 'A']++;
-            if(maximum == counter[task - 'A']){
+            counter[task-'A']++;
+            if(maximum==counter[task-'A']){
                 maxCount++;
-            }else if(maximum < counter[task - 'A']){
-                maximum = counter[task - 'A'];
+            }else if(maximum<counter[task - 'A']){
+                maximum = counter[task-'A'];
                 maxCount = 1;
-                
-            }
+            } 
         }
-        int partCount = maximum - 1;
+        int partCount = maximum-1;
         int partLength = n - (maxCount - 1);
         int emptySlots = partCount * partLength;
-        int availableTasks = tasks.length - maximum * maxCount;
+        int availableTasks = tasks.length - (maximum * maxCount);
         int idles = Math.max(0, emptySlots - availableTasks);
         return tasks.length + idles;
+        
     }
 }
