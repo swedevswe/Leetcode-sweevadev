ac40e2eb573e8fa1aa62d85a52e027bee0f79bbe
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-27 18:22:33 +0000

    Time: 1 ms (91.22%), Space: 40.5 MB (68.24%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.java
@@ -3,28 +3,29 @@ class Solution {
         int[] fib = new int[32];
         fib[0] = 1;
         fib[1] = 2;
+        
         for(int i = 2; i<32; i++){
             fib[i] = fib[i-1] + fib[i-2];
         }
-        int k = 30;
         int result = 0;
-                   
-     boolean prevBitOne = false;
-                       while(k>=0){
-                           if((n&(1<<k))!=0){
+        int k = 30;
+        
+        boolean prevBitOne = false;
+        while(k>=0){
+            if((n&(1<<k))!=0){
                 result += fib[k];
                 if(prevBitOne){
                     result--;
                     break;
-                }
-                prevBitOne = true;
-            }else{
-                               prevBitOne = false;
-                               
-                           }
-                              k--;
+                }prevBitOne = true;
+                
             
+        }else{
+                prevBitOne = false;
+            }k--;
         }
         return result + 1;
+        
+        
     }
 }
