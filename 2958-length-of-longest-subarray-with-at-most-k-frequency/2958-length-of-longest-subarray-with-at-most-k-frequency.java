ae013ffcb52669f0fa59e11d44fa33df2868fd2b
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-28 18:32:55 +0000

    Time: 79 ms (15.01%), Space: 58.8 MB (86.68%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -5,7 +5,6 @@ class Solution {
         int left = 0;
         for(int right = 0; right<nums.length; right++){
             freq.put(nums[right], freq.getOrDefault(nums[right],0)+1);
-            
             while(freq.get(nums[right])>k){
                 freq.put(nums[left], freq.get(nums[left])-1);
                 if(freq.get(nums[left])==0){
@@ -14,6 +13,7 @@ class Solution {
                 left++;
             }
             maxLen = Math.max(maxLen, right - left + 1);
+            
         }
         return maxLen;
     }
