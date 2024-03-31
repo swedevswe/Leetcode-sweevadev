25a770b1f7765d3cfd7884e004f73bb6ed90a5c8
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-03-31 07:50:33 +0100

    Time: 45 ms (53.28%), Space: 47.4 MB (41.54%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,21 +1,24 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMostKDistinct(nums, k) - atMostKDistinct(nums, k - 1);
+        return goodSubArrays(nums, k) - goodSubArrays(nums, k - 1);
     }
-    private int atMostKDistinct(int[] nums, int k){
-        Map<Integer, Integer> count = new HashMap<>();
-        int left = 0, result = 0;
+    private int goodSubArrays(int[] nums, int k){
+        Map<Integer, Integer> freq = new HashMap<>();
+        int ans = 0;
+        int left = 0;
+        
         for(int right = 0; right<nums.length; right++){
-            count.put(nums[right], count.getOrDefault(nums[right],0)+1);
-            while(count.size()>k){
-                count.put(nums[left], count.get(nums[left])-1);
-                if(count.get(nums[left])==0){
-                    count.remove(nums[left]);
+            freq.put(nums[right], freq.getOrDefault(nums[right],0)+1);
+            while(freq.size()>k){
+                freq.put(nums[left], freq.get(nums[left])-1);
+                if(freq.get(nums[left])==0){
+                    freq.remove(nums[left]);
                 }
                 left++;
+                
             }
-            result = result + right - left + 1;
+            ans = ans + right - left + 1;
         }
-        return result;
+        return ans;
     }
 }
