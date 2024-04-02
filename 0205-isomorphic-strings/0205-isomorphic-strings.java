0d561623316a74980a41f65cd83fd7de153dbd20
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-03 00:16:21 +0100

    Time: 6 ms (93.39%), Space: 42.5 MB (37.63%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        
         int[] mapS = new int[256];
         int[] mapT = new int[256];
         
@@ -14,7 +12,7 @@ class Solution {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
             
-            if(mapS[c1] != -1 && mapS[c1] != c2 || mapT[c2] != -1 && mapT[c2] != c1){
+            if(mapS[c1]!=-1&& mapS[c1]!=c2 || mapT[c2]!=-1&& mapT[c2]!=c1){
                 return false;
             }
             mapS[c1] = c2;
