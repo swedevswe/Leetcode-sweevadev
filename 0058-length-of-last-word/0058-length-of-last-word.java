46aa0b55561aefebba1d40a54f090f4a181dd881
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-03 00:17:54 +0100

    Time: 0 ms (100.00%), Space: 41.6 MB (58.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-       s =  s.trim();
+        s = s.trim();
         
-        int lastIndex = s.lastIndexOf(" ");
+        int lastIndex = s.lastIndexOf(' ');
         
         return s.length() - lastIndex - 1;
     }
