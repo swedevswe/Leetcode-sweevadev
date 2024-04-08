735975754a6b23816fcebc7fc80394ec18398d2e
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-08 22:12:34 +0100

    Time: 0 ms (100.00%), Space: 41 MB (95.78%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] count = {0, 0};
+        
+        for(int student : students){
+            count[student]++;
+        }
+        
+        int i = 0;
+        int studentsInLine = students.length;
+        
+        while(i<sandwiches.length){
+            if(count[sandwiches[i]]>0){
+                count[sandwiches[i]]--;
+                studentsInLine--;
+                i++;
+            }else{
+                break;
+            }
+        }
+        return studentsInLine;
+        
+    }
+}
