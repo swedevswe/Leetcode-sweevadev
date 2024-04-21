2c3f33da88386f65bf4b880aacf12ef24b14f8fd
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-21 16:18:06 +0100

    Time: 14 ms (56.18%), Space: 70 MB (30.71%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[][] findFarmland(int[][] land) {
-        
         List<int[]> results = new ArrayList<>();
-        int m = land.length, n = land[0].length;
+        int m = land.length; int n = land[0].length;
         
         boolean[][] visited = new boolean[m][n];
-        
         
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
@@ -17,26 +15,25 @@ class Solution {
             }
         }
         return results.toArray(new int[results.size()][]);
-        
-        
     }
     private void dfs(int[][] land, int r, int c, boolean[][] visited, int[] bounds){
-        int m = land.length;
-        int n = land[0].length;
+        int m = land.length; int n = land[0].length;
         
         if(r<0 || r>=m || c<0 || c>=n || visited[r][c] || land[r][c] == 0){
             return;
+            
         }
-        visited[r][c] = true;
+        land[r][c] = 0;
         
-        bounds[0] = Math.min(bounds[0],r);
-        bounds[1] = Math.min(bounds[1],c);
-        bounds[2] = Math.max(bounds[2],r);
-        bounds[3] = Math.max(bounds[3],c);
+        bounds[0] = Math.min(bounds[0], r);
+        bounds[1] = Math.min(bounds[1], c);
+        bounds[2] = Math.max(bounds[2], r);
+        bounds[3] = Math.max(bounds[3], c);
         
         dfs(land, r+1, c, visited, bounds);
         dfs(land, r-1, c, visited, bounds);
         dfs(land, r, c+1, visited, bounds);
         dfs(land, r, c-1, visited, bounds);
+        
     }
 }
