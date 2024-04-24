b2ea457c6aa59989e6b1a510e4c524887fc68ba5
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-24 06:40:03 +0100

    Time: 5 ms (42.24%), Space: 45.7 MB (51.24%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -3,7 +3,7 @@ class Solution {
         int rows = grid.length;
         int cols = grid[0].length;
         
-        int area = 0;
+        int area=0;
         
         for(int i = 0; i<rows; i++){
             for(int j = 0; j<cols; j++){
