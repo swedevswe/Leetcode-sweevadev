da3fb7ce9fb87332afabc4ff99b3d1590a76ecbd
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-24 19:42:14 +0100

    Time: 14 ms (48.37%), Space: 72.4 MB (11.60%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,35 +1,39 @@
 class Solution {
     public int[][] findFarmland(int[][] land) {
-        List<int[]> results = new ArrayList<>();
+        
+        List<int[]> result = new ArrayList<>();
         int m = land.length, n = land[0].length;
-        boolean[][] visited = new boolean[m][n];
+         boolean[][] visited = new boolean[m][n];
         
         for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
+            for(int j=0; j<n; j++){
                 if(land[i][j]==1 && !visited[i][j]){
                     int[] bounds = {i, j, i, j};
                     dfs(land, i, j, visited, bounds);
-                    results.add(new int[] {bounds[0], bounds[1], bounds[2], bounds[3]});
+                    result.add(new int[] {bounds[0], bounds[1], bounds[2], bounds[3]});
                 }
-            }        
+            }
         }
-        return results.toArray(new int[results.size()][]);
+        return result.toArray(new int[result.size()][]);
+        
+        
+        
     }
-    private void dfs(int[][] land, int r, int c, boolean[][]visited, int[] bounds){
+    private void dfs(int[][] land, int i, int j, boolean[][] visited, int[] bounds){
         int m = land.length, n = land[0].length;
-        if(r<0 || r>=m || c<0 || c>=n || visited[r][c] || land[r][c] == 0){
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || land[i][j]==0){
             return;
         }
-        visited[r][c] = true;
+        visited[i][j] = true;
         
-        bounds[0] = Math.min(bounds[0], r);
-        bounds[1] = Math.min(bounds[1], c);
-        bounds[2] = Math.max(bounds[2], r);
-        bounds[3] = Math.max(bounds[3], c);
+        bounds[0] = Math.min(bounds[0], i);
+        bounds[1] = Math.min(bounds[1], j);
+        bounds[2] = Math.max(bounds[2], i);
+        bounds[3] = Math.max(bounds[3], j);
         
-        dfs(land, r+1, c, visited, bounds);
-        dfs(land, r-1, c, visited, bounds);
-        dfs(land, r, c+1, visited, bounds);
-        dfs(land, r, c-1, visited, bounds);
+        dfs(land, i+1, j, visited, bounds);
+        dfs(land, i-1, j, visited, bounds);
+        dfs(land, i, j+1, visited, bounds);
+        dfs(land, i, j-1, visited, bounds);
     }
 }
