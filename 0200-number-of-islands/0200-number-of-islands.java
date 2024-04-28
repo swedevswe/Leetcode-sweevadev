7fff81ccacaec27a54285961005850c34d40e91c
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-04-28 22:19:21 +0100

    Time: 3 ms (85.46%), Space: 49.7 MB (31.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -5,11 +5,11 @@ class Solution {
         }
         int numIslands = 0;
         for(int i = 0; i<grid.length; i++){
-            for(int j = 0; j<grid[i].length; j++){
+            for(int j=0; j<grid[i].length; j++){
                 if(grid[i][j]=='1'){
-                numIslands++;
-                dfs(grid, i, j);
-                }   
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
             }
         }
         return numIslands;
@@ -19,7 +19,6 @@ class Solution {
             return;
         }
         grid[i][j] = '0';
-        
         dfs(grid, i+1, j);
         dfs(grid, i-1, j);
         dfs(grid, i, j+1);
