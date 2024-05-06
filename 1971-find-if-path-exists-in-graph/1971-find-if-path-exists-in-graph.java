2437e8b5ae1cde5c1876ad2353eabeffecd9c784
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-06 13:39:31 +0100

    Time: 53 ms (68.21%), Space: 125.2 MB (72.67%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -8,13 +8,14 @@ class Solution {
         for(int i = 0; i<n; i++){
             graph.add(new ArrayList<>());
         }
+        
         for(int[] edge : edges){
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
+        
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[n];
-        
         queue.add(source);
         visited[source] = true;
         
@@ -22,16 +23,14 @@ class Solution {
             int current = queue.poll();
             
             for(int neighbour : graph.get(current)){
-                if(neighbour==destination){
+                if(neighbour == destination){
                     return true;
                 }
                 if(!visited[neighbour]){
                     visited[neighbour] = true;
                     queue.add(neighbour);
-                    
                 }
             }
-            
         }
         return false;
     }
