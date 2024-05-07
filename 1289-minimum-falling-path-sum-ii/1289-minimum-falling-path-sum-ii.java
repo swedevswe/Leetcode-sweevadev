7cc2e344a2155d0c0cdac882781865a830858262
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-07 21:40:19 +0100

    Time: 29 ms (68.48%), Space: 51.1 MB (53.02%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,37 +1,27 @@
 class Solution {
     public int minFallingPathSum(int[][] grid) {
-        int n = grid.length;
-        int[][] dp = new int[n][n];
+        int[][] memo = new int[grid.length][grid.length];
         
-        for(int i = 0; i<n; i++){
-            dp[0][i] = grid[0][i];
+        for(int col = 0; col<grid.length; col++){
+            memo[grid.length-1][col] = grid[grid.length-1][col];
         }
         
-        for(int j = 1; j<n; j++){
-            int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
-            int minIndex = -1;
-            
-            for(int i = 0; i<n; i++){
-                if(dp[j-1][i] < min1){
-                    min2 = min1;
-                    min1 = dp[j-1][i];
-                    minIndex = i;
-                }else if(dp[j-1][i] < min2){
-                    min2 = dp[j-1][i];
+        for(int row = grid.length - 2; row>=0; row--){
+            for(int col = 0; col<grid.length; col++){
+                int nextMin = Integer.MAX_VALUE;
+                for(int nextRowCol = 0; nextRowCol<grid.length; nextRowCol++){
+                    if(nextRowCol!=col){
+                        nextMin = Math.min(nextMin, memo[row+1][nextRowCol]);
+                    }
                 }
-            }
-            for(int i = 0; i<n; i++){
-                if(i==minIndex){
-                    dp[j][i] = grid[j][i] + min2;
-                }else{
-                    dp[j][i] = grid[j][i] + min1;
-                }
+                memo[row][col] = grid[row][col] + nextMin;
             }
         }
-            int result = Integer.MAX_VALUE;
-            for(int i = 0; i<n; i++){
-                result = Math.min(result, dp[n-1][i]);
-            }
+        int result = Integer.MAX_VALUE;
+        for(int col=0; col<grid.length; col++){
+            result = Math.min(result, memo[0][col]);
+        }
         return result;
+        
     }
 }
