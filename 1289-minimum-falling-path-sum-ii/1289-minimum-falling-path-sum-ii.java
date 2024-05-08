d1e0e9792eb81949e9a63e27ddebc9d6438a992c
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-08 06:54:10 +0100

    Time: 30 ms (62.71%), Space: 51.4 MB (38.74%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,15 +1,16 @@
 class Solution {
     public int minFallingPathSum(int[][] grid) {
-        int[][] memo = new int[grid.length][grid.length];
+        int n = grid.length;
+        int[][] memo = new int[n][n];
         
-        for(int col = 0; col<grid.length; col++){
-            memo[grid.length-1][col] = grid[grid.length-1][col];
+        for(int col = 0; col<n; col++){
+            memo[n-1][col] = grid[n-1][col];
         }
         
-        for(int row = grid.length - 2; row>=0; row--){
-            for(int col = 0; col<grid.length; col++){
+        for(int row = n - 2; row>=0; row--){
+            for(int col = 0; col<n; col++){
                 int nextMin = Integer.MAX_VALUE;
-                for(int nextRowCol = 0; nextRowCol<grid.length; nextRowCol++){
+                for(int nextRowCol = 0; nextRowCol<n; nextRowCol++){
                     if(nextRowCol!=col){
                         nextMin = Math.min(nextMin, memo[row+1][nextRowCol]);
                     }
@@ -18,7 +19,7 @@ class Solution {
             }
         }
         int result = Integer.MAX_VALUE;
-        for(int col=0; col<grid.length; col++){
+        for(int col=0; col<n; col++){
             result = Math.min(result, memo[0][col]);
         }
         return result;
