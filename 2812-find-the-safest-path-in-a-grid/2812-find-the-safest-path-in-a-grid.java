1b88d1a3465afab136f5a6a3e4e656ed4fe65d74
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-15 21:38:40 +0100

    Time: 230 ms (26.30%), Space: 62.9 MB (96.81%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -3,15 +3,15 @@ class Solution {
         int n = grid.size();
         
         int[][] distance = new int[n][n];
-        
         for(int[] row : distance) Arrays.fill(row, Integer.MAX_VALUE);
-        
         int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        
+     
         Queue<int[]> queue = new LinkedList<>();
         
         for(int r = 0; r<n; r++){
             for(int c = 0; c<n; c++){
-                if(grid.get(r).get(c) == 1){
+                if(grid.get(r).get(c)==1){
                     queue.offer(new int[]{r,c});
                     distance[r][c] = 0;
                 }
@@ -24,12 +24,11 @@ class Solution {
                 int nx = x + dir[0], ny = y + dir[1];
                 if(nx >= 0 && nx < n && ny >= 0 && ny < n && distance[nx][ny] == Integer.MAX_VALUE){
                     distance[nx][ny] = distance[x][y] + 1;
-                    queue.offer(new int[] {nx, ny});
+                    queue.offer(new int[]{nx, ny});
                 }
             }
         }
-        
-        int low = 0, high = n-1;
+        int low = 0, high = n - 1;
         while(low <= high){
             int mid = (low + high) / 2;
             if(canReachSafenessFactor(distance, n, directions, mid)){
@@ -39,6 +38,10 @@ class Solution {
             }
         }
         return high;
+        
+        
+        
+        
     }
     private boolean canReachSafenessFactor(int[][] distance, int n, int[][] directions, int safenessFactor){
         if(distance[0][0] < safenessFactor) return false;
@@ -51,14 +54,15 @@ class Solution {
         while(!queue.isEmpty()){
             int[] cell = queue.poll();
             int x = cell[0], y = cell[1];
-            if(x == n-1 && y == n-1) return true;
+            if(x==n-1 && y==n-1) return true;
             
             for(int[] dir : directions){
                 int nx = x + dir[0], ny = y + dir[1];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && distance[nx][ny] >= safenessFactor){
+                if(nx >= 0 && nx < n && ny >=0 && ny < n && !visited[nx][ny] && distance[nx][ny] >= safenessFactor){
                     visited[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny});
+                    queue.offer(new int[]{nx,ny});
                 }
+                
             }
         }
         return false;
