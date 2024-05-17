ed16223ee02d31e00f06bd635696e67ec89f4a59
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-17 06:23:24 +0100

    Time: 223 ms (18.50%), Space: 63.5 MB (63.74%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -3,14 +3,14 @@ class Solution {
         int n = grid.size();
         
         int[][] distance = new int[n][n];
+        
         for(int[] row : distance) Arrays.fill(row, Integer.MAX_VALUE);
         
         int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
         
         Queue<int[]> queue = new LinkedList<>();
-        
-        for(int r = 0; r<n; r++){
-            for(int c = 0; c<n; c++){
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
                 if(grid.get(r).get(c)==1){
                     queue.offer(new int[]{r,c});
                     distance[r][c] = 0;
@@ -22,18 +22,15 @@ class Solution {
             int x = cell[0], y = cell[1];
             for(int[] dir : directions){
                 int nx = x + dir[0], ny = y + dir[1];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n && distance[nx][ny]==Integer.MAX_VALUE){
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && distance[nx][ny] == Integer.MAX_VALUE){
                     distance[nx][ny] = distance[x][y] + 1;
                     queue.offer(new int[]{nx, ny});
-                    
-                }
-                
+                } 
             }
         }
-        
-        int low = 0, high = n-1;
+        int low = 0, high = n - 1;
         while(low <= high){
-            int mid = (low+high)/2;
+            int mid = (low + high) / 2;
             if(canReachSafenessFactor(distance, n, directions, mid)){
                 low = mid + 1;
             }else{
@@ -50,6 +47,7 @@ class Solution {
         boolean[][] visited = new boolean[n][n];
         visited[0][0] = true;
         
+        
         while(!queue.isEmpty()){
             int[] cell = queue.poll();
             int x = cell[0], y = cell[1];
@@ -61,8 +59,8 @@ class Solution {
                     visited[nx][ny] = true;
                     queue.offer(new int[]{nx, ny});
                 }
-                
             }
+            
         }
         return false;
     }
