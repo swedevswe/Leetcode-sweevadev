934c40f58bd1ac686023f6308cfb559a2bb66e28
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-19 10:35:49 +0100

    Time: 9 ms (25.95%), Space: 55.5 MB (19.08%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -1,21 +1,33 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
         int n = nums.length;
-        long[][] dp = new long[n + 1][2];
-        dp[n][1] = 0;
-        dp[n][0] = Integer.MIN_VALUE;
-        
-        for (int index = n - 1; index >= 0; index--) {
-            for (int isEven = 0; isEven <= 1; isEven++) {
-                // Case 1: we perform the operation on this element.
-                long performOperation = dp[index + 1][isEven ^ 1] + (nums[index] ^ k);
-                // Case 2: we don't perform operation on this element.
-                long dontPerformOperation = dp[index + 1][isEven] + nums[index];
+        int[] netChange = new int[n];
+        long nodeSum = 0;
 
-                dp[index][isEven] = Math.max(performOperation, dontPerformOperation);
+        for (int i = 0; i < n; i++) {
+            netChange[i] = (nums[i] ^ k) - nums[i];
+            nodeSum += nums[i];
+        }
+
+        Arrays.sort(netChange);
+        // Reverse the sorted array
+        for (int i = 0; i < n / 2; i++) {
+            int temp = netChange[i];
+            netChange[i] = netChange[n - 1 - i];
+            netChange[n - 1 - i] = temp;
+        }
+
+        for (int i = 0; i < n; i += 2) {
+            // If netChange contains odd number of elements break the loop
+            if (i + 1 == n) {
+                break;
+            }
+            long pairSum = netChange[i] + netChange[i + 1];
+            // Include in nodeSum if pairSum is positive
+            if (pairSum > 0) {
+                nodeSum += pairSum;
             }
         }
-        
-        return dp[0][1];
+        return nodeSum;
     }
 }
