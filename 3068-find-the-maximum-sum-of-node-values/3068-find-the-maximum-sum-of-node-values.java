0d625c5eed52f0cd24ce5f139d0ec1e711bfad89
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-19 20:32:53 +0100

    Time: 2 ms (76.34%), Space: 54.1 MB (90.84%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -5,27 +5,26 @@ class Solution {
         int minPositiveGain = Integer.MAX_VALUE;
         int maxNegativeGain = Integer.MIN_VALUE;
         int positiveCount = 0;
+        
+        for(int num : nums){
+            int xorVal = num ^ k;
+            int gain = xorVal - num;
+            totalGain += num;
 
-        for (int num : nums) {
-            int xorValue = num ^ k;
-            int gain = xorValue - num;
-            totalSum += num;
-
-            if (gain > 0) {
-                totalGain += gain;
-                minPositiveGain = Math.min(minPositiveGain, gain);
-                positiveCount++;
-            } else {
-                maxNegativeGain = Math.max(maxNegativeGain, gain);
-            }
+        if(gain > 0){
+            totalGain += gain;
+            
+            minPositiveGain = Math.min(minPositiveGain, gain);
+            positiveCount++;
+        }else{
+            maxNegativeGain = Math.max(maxNegativeGain, gain);
+            
+            
         }
-
-        // If the number of positive gains is even, return the total sum with all gains.
-        if (positiveCount % 2 == 0) {
+        }
+        if(positiveCount % 2 == 0){
             return totalSum + totalGain;
         }
-
-        // If the number of positive gains is odd, we adjust by removing the smallest positive gain or adding the largest negative gain.
         return Math.max(totalSum + totalGain - minPositiveGain, totalSum + totalGain + maxNegativeGain);
     }
 }
