2317038eaf7cd04288233cf2c0c659f71ca7116e
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-22 08:36:20 +0100

    Time: 1 ms (100.00%), Space: 54.4 MB (52.79%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -1,21 +1,20 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        
         long totalSum = 0;
         long totalGain = 0;
         
+        int positiveCount = 0;
         int minPositiveGain = Integer.MAX_VALUE;
         int maxNegativeGain = Integer.MIN_VALUE;
-        int positiveCount = 0;
         
         for(int num : nums){
             int xorVal = num ^ k;
             int gain = xorVal - num;
             totalGain += num;
             
-            if(gain > 0){
-                totalGain += gain;
+            if(gain>0){
                 positiveCount++;
+                totalGain += gain;
                 minPositiveGain = Math.min(minPositiveGain, gain);
             }else{
                 maxNegativeGain = Math.max(maxNegativeGain, gain);
