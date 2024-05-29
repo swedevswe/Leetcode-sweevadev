18ba6cf01e00fbebf5eeb4d4d31bbe6e88d3eb5b
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-05-29 06:05:23 +0100

    Time: 1 ms (100.00%), Space: 44.1 MB (96.26%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,36 +1,35 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
+        int sLen = s.length();
+        int pLen = p.length();
         
         int sPointer = 0, pPointer = 0;
         
         int starIndex = -1, sTempIndex = -1;
         
-        while(sPointer < m){
-            if(pPointer < n && (p.charAt(pPointer) == s.charAt(sPointer) || p.charAt(pPointer) == '?')){
+        while(sPointer < sLen){
+            if(pPointer < pLen && (p.charAt(pPointer) == s.charAt(sPointer) || p.charAt(pPointer) == '?')){
                 sPointer++;
                 pPointer++;
-            }
-            else if(pPointer < n && p.charAt(pPointer) == '*') {
+                
+            }else if(pPointer < pLen && p.charAt(pPointer) == '*'){
                 starIndex = pPointer;
                 sTempIndex = sPointer;
                 pPointer++;
-            }
-            else if(starIndex != -1){
+                
+            }else if(starIndex != -1){
                 pPointer = starIndex + 1;
                 sTempIndex++;
                 sPointer = sTempIndex;
-            }
-            else{
+                
+            }else{
                 return false;
             }
         }
-        while(pPointer < n && p.charAt(pPointer) == '*'){
+        while(pPointer < pLen && p.charAt(pPointer) == '*'){
             pPointer++;
         }
-        return pPointer == n;
         
-        
+        return pPointer == pLen;
     }
 }
