04f82a5c34b0d72b1aa3125575c11a60a689ed85
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-07 21:58:35 +0100

    Time: 12 ms (79.92%), Space: 55.1 MB (69.32%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Trie trie = new Trie();
+        
+        for(String word : dictionary){
+            trie.insert(word);
+        }
+        StringBuilder sb = new StringBuilder();
+        String[] words = sentence.split(" ");
+        
+        for(String word : words){
+            String root = trie.findRoot(word);
+            sb.append(root == null ? word : root).append(" ");
+        }
+        return sb.toString().trim();
+    }
+    class Trie{
+        Trie[] children;
+        boolean isEndOfWord;
+        
+        Trie() {
+            children = new Trie[26];
+            isEndOfWord = false;
+        }
+        void insert(String word){
+            Trie node = this;
+            for(char c : word.toCharArray()){
+                int index = c - 'a';
+                if(node.children[index] == null){
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEndOfWord = true;
+        }
+        String findRoot(String word){
+            Trie node = this;
+            StringBuilder sb = new StringBuilder();
+            for(char c : word.toCharArray()){
+                int index = c - 'a';
+                if(node.children[index]== null){
+                    return null;
+                }
+                sb.append(c);
+                node = node.children[index];
+                if(node.isEndOfWord){
+                    return sb.toString();
+                }
+            }
+            return null;
+        }
+    }
+}
