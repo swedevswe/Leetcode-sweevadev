5d0f608f886cf4f5f9b18fca4291424f234ab9ec
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-15 22:11:03 +0100

    Time: 10 ms (86.29%), Space: 55.4 MB (39.41%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,9 +1,9 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
         Trie trie = new Trie();
-        
         for(String word : dictionary){
             trie.insert(word);
+            
         }
         StringBuilder sb = new StringBuilder();
         String[] words = sentence.split(" ");
@@ -17,24 +17,24 @@ class Solution {
     class Trie{
         Trie[] children = new Trie[26];
         boolean isEndOfWord = false;
-    
-    void insert(String word){
-        Trie node = this;
-        for(char c : word.toCharArray()){
-            int index = c - 'a';
-            if(node.children[index] == null){
-                node.children[index] = new Trie();
+        
+        void insert(String word){
+            Trie node = this;
+            for(char c : word.toCharArray()){
+                int index = c - 'a';
+                if(node.children[index]==null){
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
             }
-            node = node.children[index];
+            node.isEndOfWord = true;
         }
-        node.isEndOfWord = true;
-    }
         String findRoot(String word){
             Trie node = this;
             StringBuilder sb = new StringBuilder();
             for(char c : word.toCharArray()){
                 int index = c - 'a';
-                if(node.children[index] == null){
+                if(node.children[index]==null){
                     return null;
                 }
                 sb.append(c);
@@ -45,6 +45,5 @@ class Solution {
             }
             return null;
         }
-        
     }
 }
