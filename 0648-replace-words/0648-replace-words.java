0f579a101562cfd2daff09d14d70030d94e03b51
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-17 20:47:42 +0100

    Time: 10 ms (86.29%), Space: 55.5 MB (30.73%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -10,39 +10,41 @@ class Solution {
         for(String word : words){
             String root = trie.findRoot(word);
             sb.append(root == null ? word : root).append(" ");
+            
         }
         return sb.toString().trim();
+        
     }
-        class Trie{
-            Trie[] children = new Trie[26];
-            boolean isEndOfWord = false;
-            
-            void insert(String word){
-                Trie node = this;
-                for(char c : word.toCharArray()){
-                    int index = c - 'a';
-                    if(node.children[index] == null){
-                        node.children[index] = new Trie();
-                    }
-                    node = node.children[index];
+    class Trie{
+        Trie[] children = new Trie[26];
+        boolean isEndOfWord = false;
+        
+        void insert(String word){
+            Trie node = this;
+            for(char c : word.toCharArray()){
+                int index = c - 'a';
+                if(node.children[index] == null){
+                    node.children[index] = new Trie();
                 }
-                node.isEndOfWord = true;
+                node = node.children[index];
             }
-            String findRoot(String word){
-                Trie node = this;
-                StringBuilder sb = new StringBuilder();
-                for(char c : word.toCharArray()){
-                    int index = c - 'a';
-                    if(node.children[index] == null){
-                        return null;
-                    }
-                    sb.append(c);
-                    node = node.children[index];
-                    if(node.isEndOfWord){
-                        return sb.toString();
-                    }
+            node.isEndOfWord = true;
+        }
+        String findRoot(String word){
+            Trie node = this;
+            StringBuilder sb = new StringBuilder();
+            for(char c : word.toCharArray()){
+                int index = c - 'a';
+                if(node.children[index]==null){
+                    return null;
                 }
-                return null;
+                sb.append(c);
+                node = node.children[index];
+                if(node.isEndOfWord){
+                    return sb.toString();
+                }
             }
+            return null;
         }
     }
+}
