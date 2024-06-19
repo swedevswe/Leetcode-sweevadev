5bc9ef2cca3da8f8578090b4c93f1c204245a575
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-19 20:49:18 +0100

    Time: 8 ms (16.96%), Space: 40.2 MB (61.25%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
         for(int a = 0; a<=Math.sqrt(c); a++){
-            int a2 = a*a;
+            int a2 = a * a;
             int b2 = c - a2;
             if(isPerfectSquare(b2)){
                 return true;
             }
         }
         return false;
-        
     }
     private boolean isPerfectSquare(int n){
         int sqrt = (int) Math.sqrt(n);
