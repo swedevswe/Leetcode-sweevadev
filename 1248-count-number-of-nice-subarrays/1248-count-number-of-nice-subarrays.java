0e10c6a08c2a1694222c23ef6a8638cb4e68beff
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-22 19:23:46 +0100

    Time: 42 ms (13.25%), Space: 54.9 MB (45.80%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,26 +1,20 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        //sliding window technique combined with prefix sums
-        
-        //1 for odd & 0 for even = Odd Count Transformation
-        
-        //hashmap 
         
         int count = 0, prefixSum = 0;
         
         HashMap<Integer, Integer> prefixCount = new HashMap<>();
         
-        prefixCount.put(0,1);
+        prefixCount.put(0, 1);
         
         for(int num : nums){
             prefixSum += num % 2;
             
-            if(prefixCount.containsKey(prefixSum - k)){
-                count += prefixCount.get(prefixSum - k);
-            }
+        if(prefixCount.containsKey(prefixSum - k)){
+            count += prefixCount.get(prefixSum - k);
+        }
             prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
     }
 }
-
