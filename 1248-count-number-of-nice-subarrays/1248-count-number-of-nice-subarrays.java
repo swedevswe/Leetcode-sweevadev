9d455ff196d4c61dff56acd6d5d4b19183b7f2e4
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-22 19:59:15 +0100

    Time: 41 ms (16.26%), Space: 55 MB (36.41%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -10,9 +10,9 @@ class Solution {
         for(int num : nums){
             prefixSum += num % 2;
             
-        if(prefixCount.containsKey(prefixSum - k)){
-            count += prefixCount.get(prefixSum - k);
-        }
+            if(prefixCount.containsKey(prefixSum - k)){
+                count += prefixCount.get(prefixSum - k);
+            }
             prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
