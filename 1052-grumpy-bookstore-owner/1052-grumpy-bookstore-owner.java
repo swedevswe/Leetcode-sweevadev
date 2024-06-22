bc0f61f77808c20ba3c443029f831c038e124da1
Swetha Sebastian <90139229+swedevswe@users.noreply.github.com>
2024-06-22 18:55:36 +0100

    Time: 3 ms (91.51%), Space: 48 MB (6.93%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        
         int n = customers.length;
         
         int totalSatisfied = 0;
@@ -10,21 +11,22 @@ class Solution {
             }
         }
         int potentialGain = 0;
+        
         for(int i = 0; i<minutes; i++){
             if(grumpy[i] == 1){
                 potentialGain += customers[i];
             }
         }
         int maxGain = potentialGain;
+        
         for(int i = minutes; i<n; i++){
-            if(grumpy[i-minutes] == 1){
+            if(grumpy[i - minutes] == 1){
                 potentialGain -= customers[i-minutes];
             }
             if(grumpy[i] == 1){
                 potentialGain += customers[i];
             }
             maxGain = Math.max(maxGain, potentialGain);
-            
         }
         return totalSatisfied + maxGain;
     }
