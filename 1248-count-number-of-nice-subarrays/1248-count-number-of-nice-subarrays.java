228d1727768cfafb8d65880dae460563517210ca
Swe <90139229+swedevswe@users.noreply.github.com>
2024-06-25 16:02:00 +0100

    Time: 42 ms (16.48%), Space: 55.8 MB (5.88%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -13,7 +13,7 @@ class Solution {
             if(prefixCount.containsKey(prefixSum - k)){
                 count += prefixCount.get(prefixSum - k);
             }
-            prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum,0) + 1);
+            prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0) + 1);
         }
         return count;
     }
