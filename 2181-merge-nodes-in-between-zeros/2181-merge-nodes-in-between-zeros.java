b049820346f436bf49433a416c85a6948978c9e6
Swe <90139229+swedevswe@users.noreply.github.com>
2024-07-04 21:54:01 +0100

    Time: 5 ms (84.76%), Space: 81.9 MB (56.65%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,23 +10,19 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        //dummy node for head of the result list
-        //current pointer to traverse the list
-        //new current pointer to build the new list
-        //sum variable for total values between 0's
         
         ListNode dummy = new ListNode(0);
-        ListNode resultCurrent = dummy;
+        ListNode currentResult = dummy;
         
         ListNode current = head.next;
         int sum = 0;
         
-        while(current!=null){
-            if(current.val!=0){
-                sum+=current.val;
+        while(current != null){
+            if(current.val != 0){
+                sum += current.val;
             }else{
-                resultCurrent.next = new ListNode(sum);
-                resultCurrent = resultCurrent.next;
+                currentResult.next = new ListNode(sum);
+                currentResult = currentResult.next;
                 sum = 0;
             }
             current = current.next;
