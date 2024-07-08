b2f1876cbf8d5dbb65c69376a9ad7129ce5eb493
Swe <90139229+swedevswe@users.noreply.github.com>
2024-07-08 21:04:36 +0100

    Time: 0 ms (100.00%), Space: 39.9 MB (95.50%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,8 +1,6 @@
 class Solution {
     public int findTheWinner(int n, int k) {
         
-        //iterative solution
-        
         int winner = 0;
         
         for(int i = 2; i<=n; i++){
