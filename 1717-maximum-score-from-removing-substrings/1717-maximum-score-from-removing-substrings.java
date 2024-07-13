28306ff580f84769123ffa258208c1ee0a1b9a94
Swe <90139229+swedevswe@users.noreply.github.com>
2024-07-13 08:29:09 +0100

    Time: 80 ms (68.57%), Space: 45.5 MB (76.19%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -8,39 +8,35 @@ class Solution {
         }else{
             int scoreAbFirst = calculatePoints(s, 'a', 'b', x);
             String remainderAb = removeProcessed(s, 'a', 'b');
-            
             return scoreAbFirst + calculatePoints(remainderAb, 'b', 'a', y);
-            
         }
     }
-        private int calculatePoints(String s, char first, char second, int points){
-            int score = 0;
-            
-            StringBuilder remainder = new StringBuilder();
-            
-            for(char c : s.toCharArray()){
-                
-                if(c == second && remainder.length() > 0 && remainder.charAt(remainder.length() - 1) == first){
-                    remainder.deleteCharAt(remainder.length() - 1);
-                    score += points;
-                }else{
-                    remainder.append(c);
-                }
+    private int calculatePoints(String s, char first, char second, int points){
+        int score = 0;
+        
+        StringBuilder remainder = new StringBuilder();
+        
+        for(char c : s.toCharArray()){
+            if(c == second && remainder.length() > 0 && remainder.charAt(remainder.length() - 1) == first){
+                remainder.deleteCharAt(remainder.length() - 1);
+                score += points;
             }
-            return score;
-        }
-        private String removeProcessed(String s, char first, char second){
-            StringBuilder remainder = new StringBuilder();
-            
-            for(char c : s.toCharArray()){
-                if(c == second && remainder.length() > 0 && remainder.charAt(remainder.length() - 1) == first){
-                    remainder.deleteCharAt(remainder.length() - 1);
-                }else{
-                    remainder.append(c);
-                }
+            else{
+                remainder.append(c);
             }
-            return remainder.toString();
-            
         }
-            
+        return score;
+    }
+    private String removeProcessed(String s, char first, char second){
+        StringBuilder remainder = new StringBuilder();
+        
+        for(char c : s.toCharArray()){
+            if(c == second && remainder.length() > 0 && remainder.charAt(remainder.length() - 1) == first){
+                remainder.deleteCharAt(remainder.length() - 1);
+            }else{
+                remainder.append(c);
+            }
         }
+        return remainder.toString();
+    }
+}
