ca39e908e7989ce3f4f1b38b3d4f76c668b68d23
Swe <90139229+swedevswe@users.noreply.github.com>
2024-08-15 20:33:31 +0100

    Time: 2 ms (95.27%), Space: 55.5 MB (83.96%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        //keep track of 5 dollars and 10 dollars 
-        //initialise counters
-        //process each bill
-        //check for sufficient change
-        //return result
-        
         int five = 0, ten = 0;
         
         for(int bill : bills){
             if(bill == 5){
                 five++;
             }else if(bill == 10){
-                if(five == 0){
+                if(five > 0){
+                    five--;
+                    ten++;
+                }else{
                     return false;
                 }
-                five--;
-                ten++;
-            }else{
+            }else if(bill == 20){
                 if(ten > 0 && five > 0){
                     ten--;
                     five--;
@@ -31,4 +26,3 @@ class Solution {
         return true;
     }
 }
-
