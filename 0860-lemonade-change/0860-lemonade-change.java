e52dbd1d879ec6b95a57f3bb148964edd141d4c1
Swe <90139229+swedevswe@users.noreply.github.com>
2024-08-16 08:21:02 +0100

    Time: 1 ms (100.00%), Space: 55.6 MB (74.23%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -13,9 +13,9 @@ class Solution {
                     return false;
                 }
             }else if(bill == 20){
-                if(five > 0 && ten > 0){
-                    five--;
+                if(ten > 0 && five > 0){
                     ten--;
+                    five--;
                 }else if(five >= 3){
                     five -= 3;
                 }else{
