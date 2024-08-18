246cbbaed1469dd62b0c690769a9356158ff7129
Swe <90139229+swedevswe@users.noreply.github.com>
2024-08-18 12:31:33 +0100

    Time: 7 ms (75.42%), Space: 84.9 MB (27.37%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
@@ -1,6 +1,7 @@
 class Solution {
         public long maxPoints(int[][] points) {
         int m = points.length, n = points[0].length;
+        
         long[] dp = new long[n];
 
         // Initialize dp with the first row values
@@ -13,12 +14,12 @@ class Solution {
             long leftMax = dp[0];
             
         // Update dp from left to right
-            for(int j = 0; j<n; j++){
-                if(j > 0){
-                    leftMax = Math.max(leftMax, dp[j] + j);
-                }
-                newDp[j] = points[i][j] + leftMax - j;
+        for(int j = 0; j<n; j++){
+            if(j > 0){
+                leftMax = Math.max(leftMax, dp[j] + j);
             }
+            newDp[j] = points[i][j] + leftMax - j;
+        }
             long rightMax = dp[n-1] - (n-1);
         // Update dp from right to left
         for(int j = n-1; j>=0; j--){
