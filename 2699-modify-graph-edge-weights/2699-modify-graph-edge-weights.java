9fe3d6fb5568ebc91d58ee302bc2d5f7f89a1158
Swe <90139229+swedevswe@users.noreply.github.com>
2024-09-04 21:13:49 +0100

    Time: 42 ms (94.71%), Space: 54 MB (45.41%) - LeetHub

--- 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
+++ 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
@@ -5,12 +5,11 @@ class Solution {
         for(int i = 0; i < n; i++){
             List[i] = new ArrayList<>();
         }
-        
         for(int i = 0; i < edges.length; i++){
             int nodeA = edges[i][0], nodeB = edges[i][1];
             List[nodeA].add(new int[]{nodeB, i});
             List[nodeB].add(new int[]{nodeA, i});
-            }
+        }
         
         int[][] dist = new int[n][2];
         Arrays.fill(dist[source], 0);
@@ -19,38 +18,37 @@ class Solution {
                 dist[i][0] = dist[i][1] = Integer.MAX_VALUE;
             }
         }
-        dijkstra(List, edges, dist, source, 0, 0);
+        djikstra(List, edges, dist, source, 0, 0);
         int diff = target - dist[destination][0];
         if(diff < 0) return new int[][]{};
-        dijkstra(List, edges, dist, source, diff, 1);
+        djikstra(List, edges, dist, source, diff, 1);
         if(dist[destination][1] < target) return new int[][]{};
         
         for(int[] edge : edges){
             if(edge[2] == -1) edge[2] = 1;
         }
-            return edges;
-        }
+        return edges;
+    }
+    private void djikstra(List<int[]>[] List, int[][] edges, int[][] dist, int source, int diff, int run){
+        int n = List.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.add(new int[]{source, 0});
+        dist[source][run] = 0;
         
-        private void dijkstra(List<int[]>[] List, int[][] edges, int[][] dist, int source, int diff, int run){
-            int n = List.length;
-            PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-            pq.add(new int[]{source, 0});
-            dist[source][run] = 0;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int currNode = curr[0];
+            int currDist = curr[1];
             
-            while(!pq.isEmpty()){
-                int[] curr = pq.poll();
-                int currNode = curr[0];
-                int currDist = curr[1];
+            if(currDist > dist[currNode][run]) continue;
+            
+            for(int[] neighbour : List[currNode]){
+                int nextNode = neighbour[0], edgeIndex = neighbour[1];
+                int weight = edges[edgeIndex][2];
                 
-                if(currDist > dist[currNode][run]) continue;
+                if(weight == -1) weight = 1;
                 
-                for(int[] neighbour : List[currNode]){
-                    int nextNode = neighbour[0], edgeIndex = neighbour[1];
-                    int weight = edges[edgeIndex][2];
-                    
-                    if(weight == -1) weight = 1;
-                    if(run == 1 && edges[edgeIndex][2] == -1){
-                    
+                if(run == 1 && edges[edgeIndex][2] == -1){
                     int newWeight = diff + dist[nextNode][0] - dist[currNode][1];
                     if(newWeight > weight){
                         edges[edgeIndex][2] = weight = newWeight;
@@ -59,7 +57,9 @@ class Solution {
                 if(dist[nextNode][run] > dist[currNode][run] + weight){
                     dist[nextNode][run] = dist[currNode][run] + weight;
                     pq.add(new int[]{nextNode, dist[nextNode][run]});
-                }
+                    }
+                
+                
             }
         }
     }
