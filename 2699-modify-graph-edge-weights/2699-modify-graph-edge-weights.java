4ce1bdecbb784080fb60deee1d44b5e598e47fba
Swe <90139229+swedevswe@users.noreply.github.com>
2024-09-22 20:27:25 +0100

    Time: 44 ms (68.78%), Space: 54.5 MB (34.38%) - LeetHub

--- 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
+++ 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
         List<int[]>[] List = new ArrayList[n];
-        
         for(int i = 0; i < n; i++){
             List[i] = new ArrayList<>();
         }
-        
         for(int i = 0; i < edges.length; i++){
-            int a = edges[i][0], b = edges[i][1];
-            List[a].add(new int[]{b, i});
-            List[b].add(new int[]{a, i});
+            int nodeA = edges[i][0], nodeB = edges[i][1];
+            List[nodeA].add(new int[]{nodeB, i});
+            List[nodeB].add(new int[]{nodeA, i});
         }
-        
         int[][] dist = new int[n][2];
         Arrays.fill(dist[source], 0);
         for(int i = 0; i < n; i++){
@@ -19,17 +16,17 @@ class Solution {
                 dist[i][0] = dist[i][1] = Integer.MAX_VALUE;
             }
         }
-        dijkstra(List, edges, dist, source, 0, 0);
-        int diff = target - dist[destination][0];
-        if(diff < 0) return new int[][]{};
-        dijkstra(List, edges, dist, source, diff, 1);
-        if(dist[destination][1] < target) return new int[][]{};
-        
-        for(int[] edge : edges){
-            if(edge[2] == -1) edge[2] = 1;
-        }
-        return edges; 
+    dijkstra(List, edges, dist, source, 0, 0);
+    int diff = target - dist[destination][0];
+    if(diff < 0) return new int[][]{};
+    dijkstra(List, edges, dist, source, diff, 1);
+    if(dist[destination][1] < target) return new int[][]{};
+    
+    for(int[] edge : edges){
+        if(edge[2] == -1) edge[2] = 1;
     }
+    return edges;
+}
     private void dijkstra(List<int[]>[] List, int[][] edges, int[][] dist, int source, int diff, int run){
         int n = List.length;
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
@@ -48,7 +45,6 @@ class Solution {
                 int weight = edges[edgeIndex][2];
                 
                 if(weight == -1) weight = 1;
-                
                 if(run == 1 && edges[edgeIndex][2] == -1){
                     int newWeight = diff + dist[nextNode][0] - dist[currNode][1];
                     if(newWeight > weight){
@@ -61,5 +57,6 @@ class Solution {
                 }
             }
         }
+                                                      
     }
 }
