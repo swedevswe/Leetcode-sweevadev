375fd9ef8eaae7b8c40737870090092d3cbf269f
Swe <90139229+swedevswe@users.noreply.github.com>
2024-11-16 05:10:16 +0000

    Time: 1 ms (100.00%), Space: 64.7 MB (75.00%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -2,7 +2,7 @@ class Solution {
     public int findLengthOfShortestSubarray(int[] arr) {
         int n = arr.length;
         int left = 0;
-        while(left < n - 1 && arr[left] <= arr[left + 1]){
+        while(left < n - 1 && arr[left] <= arr[left+1]){
             left++;
         }
         if(left == n - 1){
