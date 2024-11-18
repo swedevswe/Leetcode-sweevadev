a18ab98455fdb6de408d0c79574b5f8d9dc21074
Swe <90139229+swedevswe@users.noreply.github.com>
2024-11-18 12:43:07 +0000

    Time: 0 ms (100.00%), Space: 42.2 MB (55.38%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,32 +1,23 @@
 class Solution {
+
     public int[] decrypt(int[] code, int k) {
-        int n = code.length;
-        int[] result = new int[n];
-        if(k == 0) return result;
-        int[] doubled = new int[n * 2];
-        for(int i = 0; i < n * 2; i++){
-            doubled[i] = code[i % n];
+        int[] result = new int[code.length];
+        if (k == 0) return result;
+        // Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        // If k < 0, the starting point will be end of the array.
+        if (k < 0) {
+            start = code.length - Math.abs(k);
+            end = code.length - 1;
         }
-        int windowSum = 0;
-        if(k > 0){
-            for(int i = 1; i <= k; i++){
-                windowSum += doubled[i];
-            }
-            for(int i = 0; i < n; i++){
-                result[i] = windowSum;
-                windowSum -= doubled[i + 1];
-                windowSum += doubled[i + k + 1];
-            }
-        }else{
-            k = -k;
-            for(int i = n - k; i < n; i++){
-                windowSum += doubled[i];
-            }
-            for(int i = 0; i < n; i++){
-                result[i] = windowSum;
-                windowSum -= doubled[n - k + i];
-                windowSum += doubled[n + i];
-            }
+        for (int i = start; i <= end; i++) sum += code[i];
+        // Scan through the code array as i moving to the right, update the window sum.
+        for (int i = 0; i < code.length; i++) {
+            result[i] = sum;
+            sum -= code[(start) % code.length];
+            sum += code[(end + 1) % code.length];
+            start++;
+            end++;
         }
         return result;
     }
