57e77d1c00be8506764a63253ce987a53a59f413
Swe <90139229+swedevswe@users.noreply.github.com>
2024-11-19 11:38:57 +0000

    Time: 39 ms (66.46%), Space: 57.2 MB (84.54%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,11 +1,10 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long maxSum = 0;
         long currSum = 0;
+        long maxSum = 0;
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.length; i++){
             currSum += nums[i];
-            
             while(set.contains(nums[i])){
                 currSum -= nums[i - set.size()];
                 set.remove(nums[i - set.size()]);
