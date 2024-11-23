eb7c58dffe06f9eba711ef7b93727660de427361
Swe <90139229+swedevswe@users.noreply.github.com>
2024-11-23 20:24:48 +0000

    Time: 6 ms (97.58%), Space: 79.2 MB (85.71%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,28 +1,26 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        int m = box.length; 
+        int m = box.length;
         int n = box[0].length;
-        char[][] rotated = new char[n][m];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                rotated[j][m - i - 1] = box[i][j];
+        char[][] result = new char[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                result[i][j] = '.';
             }
         }
-        for(int col = 0; col < m; col++){
-            int emptyRow = n - 1;
-            for(int row = n - 1; row >= 0; row--){
-                if(rotated[row][col] == '#'){
-                    char temp = rotated[row][col];
-                    rotated[row][col] = '.';
-                    rotated[emptyRow][col] = temp;
-                    emptyRow--;
-                }else if(rotated[row][col] == '*'){
-                    emptyRow = row - 1;
-                }else{
-                    rotated[row][col] = '.';
+        for(int i = 0; i < m; i++){
+            int lowestRowWithEmptyCell = n - 1;
+            for(int j = n - 1; j >= 0; j--){
+                if(box[i][j] == '#'){
+                    result[lowestRowWithEmptyCell][m - i - 1] = '#';
+                    lowestRowWithEmptyCell--;
+                }
+                if(box[i][j] == '*'){
+                    result[j][m - i - 1] = '*';
+                    lowestRowWithEmptyCell = j - 1;
                 }
             }
         }
-        return rotated;
+        return result;
     }
 }
