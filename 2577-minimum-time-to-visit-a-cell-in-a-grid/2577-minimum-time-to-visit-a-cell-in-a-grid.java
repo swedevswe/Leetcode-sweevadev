b974da13f43e7c8418eb1a945155a45486530ae2
Swe <90139229+swedevswe@users.noreply.github.com>
2024-12-01 19:48:12 +0000

    Time: 113 ms (91.32%), Space: 57.2 MB (68.79%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.java
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.java
@@ -2,12 +2,12 @@ class Solution {
     private static final int[][] DIRECTIONS = {{-1,0},{0,1},{1,0},{0,-1}};
     public int minimumTime(int[][] grid) {
         if(grid[0][1] > 1 && grid[1][0] > 1){
-           return -1; 
-        } 
+            return -1;
+        }
         int m = grid.length;
         int n = grid[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
         boolean[][] visited = new boolean[m][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]); //min-heap
         pq.offer(new int[]{0,0,0});
         visited[0][0] = true;
         while(!pq.isEmpty()){
@@ -16,22 +16,21 @@ class Solution {
             for(int[] dir : DIRECTIONS){
                 int newRow = row + dir[0];
                 int newCol = col + dir[1];
-                if(newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || visited[newRow][newCol]) {
+                if(newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || visited[newRow][newCol]){
                     continue;
-            }
-                    int nextTime = currTime + 1;
-                    if(grid[newRow][newCol] > nextTime){
-                        int waitTime = ((grid[newRow][newCol] - nextTime + 1) / 2) * 2;
-                        nextTime += waitTime;
-                    }
-                    if(newRow == m - 1 && newCol == n - 1){
-                        return nextTime;
-                    }
-            visited[newRow][newCol] = true;
-            pq.offer(new int[]{nextTime, newRow, newCol});
                 }
+                int newTime = currTime + 1;
+                if(grid[newRow][newCol] > newTime){
+                    int waitTime = ((grid[newRow][newCol] - newTime + 1) / 2) * 2;
+                    newTime += waitTime;
+                }
+                if(newRow == m - 1 && newCol == n - 1){
+                    return newTime;
+                }
+                visited[newRow][newCol] = true;
+                pq.offer(new int[]{newTime, newRow, newCol});
             }
-    return -1;
+        }
+        return -1;
     }
 }
-
