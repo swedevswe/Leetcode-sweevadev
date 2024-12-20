a65be37cf3d32522e892569f3b881ac305d7db49
Swe <90139229+swedevswe@users.noreply.github.com>
2024-12-20 22:35:17 +0000

    Time: 1 ms (72.63%), Space: 48.5 MB (55.49%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,35 +15,19 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-       //reverse the node values of 1, 3, 5, 7, 9 etc. levels, swap at the same level.
-        //odd levels only
-        //perfect binary tree = all nodes filled equally at every level.
-        //BFS with queue - isolate odd levels.
-    if(root == null) return null;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int level = 0;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<TreeNode> currentLevelNodes = new ArrayList<>();
-            for(int i = 0; i < size; i++){
-                TreeNode node = queue.poll();
-                currentLevelNodes.add(node);
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
-            }
-            if(level % 2 == 1){
-                int left = 0, right = currentLevelNodes.size() - 1;
-                while(left < right){
-                    int temp = currentLevelNodes.get(left).val;
-                    currentLevelNodes.get(left).val = currentLevelNodes.get(right).val;
-                    currentLevelNodes.get(right).val = temp;
-                    left++;
-                    right--;
-                }
-            }
-            level++;
-        }
+        //DFS
+        if(root == null) return null;
+        reverseOddLevels(root.left, root.right, 1);
         return root;
+    }
+    private void reverseOddLevels(TreeNode left, TreeNode right, int level){
+        if(left == null || right == null) return;
+        if(level % 2 == 1){
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+        reverseOddLevels(left.left, right.right, level + 1);
+        reverseOddLevels(left.right, right.left, level + 1);
     }
 }
